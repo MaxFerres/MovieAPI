f0185e627e522c65a2fd591f4dd27fc231a12c24
Maximilian Ferres <135696129+MaxFerres@users.noreply.github.com>
2024-06-12 23:37:25 +0200

    DB connected, Querytest

--- src/main/java/com/test/movies/Review.java
+++ src/main/java/com/test/movies/Review.java
@@ -4,6 +4,7 @@ package com.test.movies;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
---
+++ src/main/java/com/test/movies/ReviewRepository.java
@@ -0,0 +1,8 @@
+package com.test.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
